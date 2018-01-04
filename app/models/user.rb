8d3fe822065d7e522399cd7c685cb288ea78177b
David Do <daviddo@davids-mbp.uphs.upenn.edu>
2018-01-04 11:41:30 -0500

    reports shows creators name

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,8 @@ class User
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def display_name
+   "#{ self.first_name } #{ self.last_name }".strip
+  end
 end
