2bcb7a93b67cf2e740b730b6f64ee9afde1367e9
David Do <daviddo@Davids-MacBook-Pro.local>
2018-02-23 22:32:20 -0500

    messages resource enforce auth

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show_by_key]
 
   def show_by_key
     @message = Message.find_by(key: params[:key])
